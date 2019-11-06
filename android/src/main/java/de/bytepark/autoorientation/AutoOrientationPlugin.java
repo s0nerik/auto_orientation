c279925595b67eb66824c7533366037112aabf0e
Alex Isaienko <thesonerik@gmail.com>
2019-11-06 11:45:42 +0200

    Update AutoOrientationPlugin.java
    Null activity handling

--- android/src/main/java/de/bytepark/autoorientation/AutoOrientationPlugin.java
+++ android/src/main/java/de/bytepark/autoorientation/AutoOrientationPlugin.java
@@ -3,6 +3,7 @@ package de.bytepark.autoorientation;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
+import android.util.Log;
 
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -12,52 +13,54 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** AutoOrientationPlugin */
 public class AutoOrientationPlugin implements MethodCallHandler {
-  Activity activity;
-
+  private static Registrar sRegistrar;
+  
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
+    sRegistrar = registrar;
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "auto_orientation");
-    channel.setMethodCallHandler(new AutoOrientationPlugin(registrar.activity()));
-  }
-
-  private AutoOrientationPlugin(Activity activity) {
-    this.activity = activity;
+    channel.setMethodCallHandler(new AutoOrientationPlugin());
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
+    Activity activity = registrar.activity();
+    if (activity == null) {
+      Log.e("AutoOrientationPlugin", "activity == null");
+      return;
+    }
     switch(call.method) {
       case "setLandscapeRight":
-        this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         break;
       case "setLandscapeLeft":
-        this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
         break;
       case "setPortraitUp":
-        this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         break;
       case "setPortraitDown":
-        this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
         break;
       case "setPortraitAuto":
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
+          activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
         } else {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+          activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
         }
         break;
       case "setLandscapeAuto":
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
+          activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
         } else {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+          activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
         }
         break;
       case "setAuto":
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_USER);
+          activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_USER);
         } else {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+          activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
         }
         break;
       default:
